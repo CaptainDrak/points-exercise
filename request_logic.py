698f46665440bc029834a5889b1466bf97eced9d
Creed Robbins <ccrinbama@gmail.com>
2021-10-28 12:27:03 -0500

    spend endpoint and error handling
    logic for the spend endpoint, and error handling surrounding trying to spend more points than exist, and sending a payer into negative points

--- request_logic.py
+++ request_logic.py
@@ -1,4 +1,7 @@
-def calculate_balance(transactions):
+#calculates existing balances for all payers
+#accepts a list of transactions
+#returns a list of balances for each payer
+def calculate_balances(transactions):
     balance = {}
     for i in transactions:
         if i['payer'] in balance.keys():
@@ -7,8 +10,21 @@ def calculate_balance(transactions):
             balance[i['payer']] = i['points']
     return balance
 
+#calculates total point balance across all payers
+#accepts a list of transactions
+#returns a point total integer
+def calculate_total_balance(transactions):
+    total_balance = 0
+    for i in transactions:
+        total_balance += i['points']
+    return total_balance
+
+
+#checks for potential negative values for payer totals
+#accepts a list of transactions and a transaction request
+#returns True/False and point total integer for that payer
 def check_for_negative_balance(transactions, request):
-    balance = calculate_balance(transactions)
+    balance = calculate_balances(transactions)
     if request['payer'] not in balance.keys() and request['points'] >= 0:
         return False, 0
     elif request['payer'] not in balance.keys() and request['points'] < 0:
@@ -17,3 +33,27 @@ def check_for_negative_balance(transactions, request):
         if (balance[request['payer']] + request['points']) < 0:
             return True, balance[request['payer']]
         else: return False, balance[request['payer']]
+
+#spends a requested total of points
+#accepts a list of transactions and a points integer
+#returns a list of transations
+def spend_points(sorted_transactions, points):
+    counter = 0
+    while points > 0:
+        for i in sorted_transactions:
+            if (points - i['points']) > 0:
+                points = points - i['points']
+                i['points'] = 0
+                counter+=1
+            elif (points - i['points']) == 0:
+                i['points'] = 0
+                del sorted_transactions[0:counter]
+                points = 0
+                del sorted_transactions[0:counter]
+                break
+            elif (points - i['points']) < 0:
+                i['points'] = i['points'] - points
+                del sorted_transactions[0:counter]
+                points = 0
+                break
+    return sorted_transactions
--- requests.py
+++ requests.py
@@ -6,8 +6,9 @@ app = Flask(__name__)
 transactions = []
 
 @app.route('/add', methods=['POST'])
-def add_transaction():
+def add():
     json = request.get_json()
+
     negative_check = rl.check_for_negative_balance(transactions, json)
     if negative_check[0] == False:
         transactions.append(request.get_json())
@@ -16,10 +17,22 @@ def add_transaction():
         return f'Payer total cannot be negative.\nCurrent payer total is {negative_check[1]}.', 400
 
 @app.route('/spend', methods=['POST'])
-def spend_points():
+def spend():
+    json = request.get_json(cache=True)
+    points = json['points']
     sorted_transactions = sorted(transactions, key=lambda d: d['timestamp'])
-    return jsonify(sorted_transactions)
+    total_balance = rl.calculate_total_balance(sorted_transactions)
+
+    if total_balance < points:
+        return f'Requested {points} points be spent, but there are only {total_balance} points available. Cannot request to spend more points than are available.', 400
+    else:
+        transactions.clear()
+        new_transactions = rl.spend_points(sorted_transactions, points)
+        for i in new_transactions:
+            transactions.append(i)
+        return f'{points} points spent successfully.'
+    
 
 @app.route('/balance')
-def get_balance():
-    return rl.calculate_balance(transactions)
+def get():
+    return rl.calculate_balances(transactions)
