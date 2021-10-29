e9e9d5adb221fccebf3a7a4f1a749e1223af748d
Creed Robbins <ccrinbama@gmail.com>
2021-10-29 18:54:26 -0500

    move logic from requests
    move the logic from the actual requests to the request_logic file

--- request_logic.py
+++ request_logic.py
@@ -1,7 +1,11 @@
+import request_json_validation as rj
+
+transactions = []
+
 #calculates existing balances for all payers
 #accepts a list of transactions
 #returns a list of balances for each payer
-def calculate_balances(transactions):
+def calculate_balances():
     point_balances = {}
     for i in transactions:
         if i['payer'] in point_balances.keys():
@@ -13,47 +17,85 @@ def calculate_balances(transactions):
 #calculates total point balance across all payers
 #accepts a list of transactions
 #returns a point total integer
-def calculate_total_balance(transactions):
+def calculate_total_balance(passed_transactions):
     total_balance = 0
-    for i in transactions:
+    for i in passed_transactions:
         total_balance += i['points']
     return total_balance
 
 
 #checks for potential negative values for payer totals
-#accepts a list of transactions and a transaction request
+#accepts a transaction request
 #returns True/False and point total integer for that payer
-def check_for_negative_balance(transactions, request):
-    point_balances = calculate_balances(transactions)
-    if request['payer'] not in point_balances.keys() and request['points'] >= 0:
+def check_for_negative_balance(request):
+    payer_point_balances = calculate_balances()
+    if request['payer'] not in payer_point_balances.keys() and request['points'] >= 0:
         return False, 0
-    elif request['payer'] not in point_balances.keys() and request['points'] < 0:
+    elif request['payer'] not in payer_point_balances.keys() and request['points'] < 0:
         return True, 0
-    elif request['payer'] in point_balances.keys():
-        if (point_balances[request['payer']] + request['points']) < 0:
-            return True, point_balances[request['payer']]
-        else: return False, point_balances[request['payer']]
+    elif request['payer'] in payer_point_balances.keys():
+        if (payer_point_balances[request['payer']] + request['points']) < 0:
+            return True, payer_point_balances[request['payer']]
+        else: return False, payer_point_balances[request['payer']]
 
 #spends a requested total of points
 #accepts a list of transactions and a points integer
 #returns a list of transations
-def spend_points(sorted_transactions, points):
+def point_spender(passed_sorted_transactions, points):
     counter = 0
     while points > 0:
-        for i in sorted_transactions:
+        for i in passed_sorted_transactions:
             if (points - i['points']) > 0:
                 points = points - i['points']
                 i['points'] = 0
                 counter+=1
             elif (points - i['points']) == 0:
                 i['points'] = 0
-                del sorted_transactions[0:counter]
+                del passed_sorted_transactions[0:counter+1]
                 points = 0
-                del sorted_transactions[0:counter]
                 break
             elif (points - i['points']) < 0:
                 i['points'] = i['points'] - points
-                del sorted_transactions[0:counter]
+                del passed_sorted_transactions[0:counter]
                 points = 0
                 break
-    return sorted_transactions
+    return passed_sorted_transactions
+
+#carries out collective logic required for add endpoint
+#accepts a request
+#returns an error if one is thrown, or a successful point addition response
+def add_points(add_request):
+    json_validation = rj.add_json_validation(add_request)
+
+    if json_validation != None:
+        return json_validation, 400
+
+    negative_check = check_for_negative_balance(add_request)
+    if negative_check[0] == False:
+        transactions.append(add_request)
+        return f'Transaction added:\n{transactions[-1]}'
+    else: 
+        return f'Payer total cannot be negative.\nCurrent payer total is {negative_check[1]}.', 400
+
+#carries out collective logic required for spend endpoint
+#accepts a request
+#returns an error if one is thrown, or a successful point spending response
+def spend_points(spend_request):
+    json_validation = rj.spend_json_validation(spend_request)
+
+    if json_validation != None:
+        return json_validation, 400
+    
+    points = spend_request['points']
+    sorted_transactions = sorted(transactions, key=lambda d: d['timestamp'])
+    total_balance = calculate_total_balance(sorted_transactions)
+    json_validation = rj.spend_json_validation(spend_request)
+
+    if total_balance < points:
+        return f'Requested {points} points be spent, but there are only {total_balance} points available. Cannot request to spend more points than are available.', 400
+    else:
+        transactions.clear()
+        new_transactions = point_spender(sorted_transactions, points)
+        for i in new_transactions:
+            transactions.append(i)
+        return f'{points} points spent successfully.'
--- requests.py
+++ requests.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request
 import request_logic as rl
 import request_json_validation as rj
 
@@ -6,44 +6,17 @@ app = Flask(__name__)
 
 transactions = []
 
+#route through which points are added for a specific payer
 @app.route('/add', methods=['POST'])
 def add():
-    json = request.get_json()
-    json_validation = rj.add_json_validation(json)
+    return rl.add_points(request.get_json(cache=True))
 
-    if json_validation != None:
-        return json_validation, 400
-
-    negative_check = rl.check_for_negative_balance(transactions, json)
-    if negative_check[0] == False:
-        transactions.append(request.get_json())
-        return f'Transaction added:\n{transactions[-1]}'
-    else: 
-        return f'Payer total cannot be negative.\nCurrent payer total is {negative_check[1]}.', 400
-
+#route through which a requested amount of points is spent
 @app.route('/spend', methods=['POST'])
 def spend():
-    json = request.get_json(cache=True)
-    json_validation = rj.spend_json_validation(json)
-
-    if json_validation != None:
-        return json_validation, 400
+    return rl.spend_points(request.get_json(cache=True))
     
-    points = json['points']
-    sorted_transactions = sorted(transactions, key=lambda d: d['timestamp'])
-    total_balance = rl.calculate_total_balance(sorted_transactions)
-    json_validation = rj.spend_json_validation(json)
-
-    if total_balance < points:
-        return f'Requested {points} points be spent, but there are only {total_balance} points available. Cannot request to spend more points than are available.', 400
-    else:
-        transactions.clear()
-        new_transactions = rl.spend_points(sorted_transactions, points)
-        for i in new_transactions:
-            transactions.append(i)
-        return f'{points} points spent successfully.'
-    
-
+#route through which a request for all point balances by payer is returned
 @app.route('/balance')
 def get():
-    return rl.calculate_balances(transactions)
+    return rl.calculate_balances()
