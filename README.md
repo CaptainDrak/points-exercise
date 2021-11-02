94df5596d1576fc3bd80ec38dd80e2ac9a813193
Creed Robbins <ccrinbama@gmail.com>
2021-11-02 16:34:39 -0500

    spent response formatting, readme, bugs
    added logic for formatting spent points responses, created the readme, and fixed some bugs related to returning point balances with zero-sum payers present

--- README.md
+++ README.md
@@ -1,1 +1,85 @@
+
 # points-exercise
+
+points-exercise is a web service that creates a log of point accumulation transactions consisting of a payer, point total, and timestamp, allows for the spending of said points, and reports point totals by payer.
+
+
+## Installation
+
+If you don't have it already, you'll need to install Docker. You can find the instructions to do so here: https://docs.docker.com/get-docker/
+
+After docker has been installed, pull this repo and navigate to it on your machine using terminal, command prompt, etc. Once there, run these commands:
+
+```bash
+  docker build -t points .
+  docker run -p 5000:5000 points
+```
+This will run a docker container containing the points-exercise service. You can then make requests to the endpoints indicated below.
+## Endpoints
+
+The base URL for all requests when being made to the docker container is:
+```
+http://localhost:5000
+```
+<details>
+<summary> Add transaction</summary>
+
+#### **Add transaction**
+
+```http
+  POST /add
+```
+Adds a transaction for a specific payer. Responds with a copy of the transaction that was added.  
+
+Structure:  
+```
+{
+  "payer": <string>
+  "points": <integer>
+  "timestamp": <date>
+}
+```
+Example:  
+```
+{
+  "payer": "Let's Potato Chips"
+  "points": 5000
+  "timestamp": "2020-09-04T12:30:00Z"
+}
+```
+</details>
+
+<details>
+<summary> Spend points</summary>
+
+#### **Spend points**
+
+```
+  POST /spend
+```
+Spends a requested amount of points. Points from the oldest (by timestamp) transactions will be spent first. Responds with a list of all total points spent by each payer.  
+
+Structure:
+```
+{
+  points: <integer>
+}
+```
+Example:
+```
+{
+  points: 2000
+}
+```
+</details>
+
+<details>
+<summary>Get balances</summary>
+
+#### **Get balances**
+
+```
+  GET /balances
+```
+Responds with a list of individual balances for each payer.
+</details>
--- request_logic.py
+++ request_logic.py
@@ -1,10 +1,11 @@
 import request_json_validation as rj
+from flask import jsonify
 
 transactions = []
 
 #calculates existing balances for all payers
 #accepts a list of transactions
-#returns a list of balances for each payer
+#returns a dict of balances for each payer
 def calculate_balances():
     point_balances = {}
     for i in transactions:
@@ -12,6 +13,16 @@ def calculate_balances():
             point_balances[i['payer']] = point_balances[i['payer']] + i['points']
         else:
             point_balances[i['payer']] = i['points']
+
+    zero_sum_payers = []
+
+    for i in point_balances:
+        if point_balances[i] == 0:
+            zero_sum_payers.append(i)
+    
+    for i in zero_sum_payers:
+        point_balances.pop(i)
+
     return point_balances
 
 #calculates total point balance across all payers
@@ -23,6 +34,21 @@ def calculate_total_balance(passed_transactions):
         total_balance += i['points']
     return total_balance
 
+#calculates total balance spent, based on differences in passed balances
+#accepts two dicts of balances (identical to returned value of calculate_balances())
+#returns a jsonified dict of spent balances by payer
+def calculate_spent_balances(original_balance, new_balance):
+    spent_points = {}
+    spent_points_list = []
+    for i in original_balance:
+        if i not in new_balance:
+            spent_points[i] = 0 - original_balance[i]
+        elif i in new_balance and original_balance[i] != new_balance[i]:
+            spent_points[i] = new_balance[i] - original_balance[i]
+    for i in spent_points:
+        spent_points_list.append({"payer": i, "points": spent_points[i]})
+    response = jsonify(spent_points_list)
+    return response
 
 #checks for potential negative values for payer totals
 #accepts a transaction request
@@ -37,6 +63,7 @@ def check_for_negative_balance(request):
         if (payer_point_balances[request['payer']] + request['points']) < 0:
             return True, payer_point_balances[request['payer']]
         else: return False, payer_point_balances[request['payer']]
+
 
 #spends a requested total of points
 #accepts a list of transactions and a points integer
@@ -61,6 +88,7 @@ def point_spender(passed_sorted_transactions, points):
                 break
     return passed_sorted_transactions
 
+
 #carries out collective logic required for add endpoint
 #accepts a request
 #returns an error if one is thrown, or a successful point addition response
@@ -77,6 +105,7 @@ def add_points(add_request):
     else: 
         return f'Payer total cannot be negative.\nCurrent payer total is {negative_check[1]}.', 400
 
+
 #carries out collective logic required for spend endpoint
 #accepts a request
 #returns an error if one is thrown, or a successful point spending response
@@ -86,11 +115,14 @@ def spend_points(spend_request):
     if json_validation != None:
         return json_validation, 400
     
+    original_balance = calculate_balances()
     points = spend_request['points']
     sorted_transactions = sorted(transactions, key=lambda d: d['timestamp'])
     total_balance = calculate_total_balance(sorted_transactions)
     json_validation = rj.spend_json_validation(spend_request)
 
+    if points <= 0:
+        return f'Must request a positive, non-negative amount of points to spend.', 400
     if total_balance < points:
         return f'Requested {points} points be spent, but there are only {total_balance} points available. Cannot request to spend more points than are available.', 400
     else:
@@ -98,4 +130,5 @@ def spend_points(spend_request):
         new_transactions = point_spender(sorted_transactions, points)
         for i in new_transactions:
             transactions.append(i)
-        return f'{points} points spent successfully.'
+        new_balance = calculate_balances()
+        return calculate_spent_balances(original_balance, new_balance)
